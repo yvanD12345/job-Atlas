6a04c0aa11c0200ccf91bb2a949900ad34b8814f
noble237 <75092902+noble237@users.noreply.github.com>
2022-10-11 08:26:51 -0400

    test recherche

--- tests/Recherche.test.js
+++ tests/Recherche.test.js
@@ -1,9 +1,22 @@
+// jest.setTimeout(7500)
 const db = require('../connexion')
-const Recherche = require('../fonctions/recherche')
+const Emploi = require('../models/Offre_emploi')
+const rechercheMethodes = require('../fonctions/recherche');
 
 
 beforeAll(async () => await db.connexion())
- afterEach(async () => await db.effacerBD())
+//afterEach(async () => await db.effacerBD())
  afterAll(async () => await db.deconnexionBD())
 
+// Le describe sert à mettre les tests similaires ensemble dans un même endroit
+//Ceci est un test régulier avec JEST
+describe('Recherche emploi', () => {
+    it('Doit trouver un emploi/stage dans la base de données', async () => {
+        const mot = rechercheMethodes.escapeRegex(programmeur);
+        const resultat = rechercheMethodes.rechercheMotCle(mot);
+        console.log(testEmploi)
+        console.log(resultat)
+        expect((await mot)).toBe(resultat.Titre_emploi);
+    })
 
+})
